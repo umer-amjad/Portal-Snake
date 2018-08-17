4208bed8e4fe61ee44cf6c722a7b034794356bae
Umer Amjad <m.amjad.umer@gmail.com>
2018-08-16 20:14:48 -0400

    Started implementing option setting function, with error handling as required

--- main.cc
+++ main.cc
@@ -1,6 +1,7 @@
 #include <thread>
 #include <chrono>
 #include <iostream>
+#include <exception>
 
 #include <ncurses.h>
 
@@ -19,7 +20,7 @@ Input charToInput(int in) {
 
 }
 
-int main(int argc, char** argv) {
+struct Options {
     int height = 30;
     int width = 40;
     int speed = 11;
@@ -33,8 +34,86 @@ int main(int argc, char** argv) {
             {{10, 35}, {15, 14}}, 
             {{25, 25}, {14, 15}}, 
             {{5, 15}, {15, 16}}};
-    Board b(height, width, speed, starting_length, enlargement, borders_on, invincible, portals);
-    View v(height, width, &b, fps);
+};
+
+//helper function gets number between bounds
+int readInt(int lower, int upper){
+    std::string input;
+    std::getline(std::cin, input);
+    int val = stoi(input);
+    if (val < lower || upper < val) {
+        throw (std::out_of_range("The number entered is out of range. Please enter a number between "
+                + std::to_string(lower) + " and " + std::to_string(upper) + " (inclusive). "));
+    }
+    return stoi(input);
+}
+
+void setOptions(Options& o) {
+    std::cout << "Current options. Enter the associated number to edit \n";
+    std::cout << "0 - Height: " << o.height << '\n';
+    std::cout << "1 - Width: " << o.width << '\n'; 
+    std::cout << "2 - Snake speed: " << o.speed << '\n';
+    std::cout << "3 - Snake starting length: " << o.starting_length << '\n';
+    std::cout << "4 - Enlargement factor: " << o.enlargement << '\n';
+    std::cout << "5 - Borders: " << o.borders_on << '\n';
+    std::cout << "6 - Invincibility mode: " << o.invincible << '\n';
+    std::cout << "7 - Frames per second: " << o.fps << '\n';
+    std::cout << "8 - Portal pairs, " << o.portals.size() << ": \n";
+    for (const auto& portal_pair : o.portals) {
+        std::cout << "  (" << portal_pair.first.r << ", " 
+                << portal_pair.first.c << "), "
+                << "(" << portal_pair.second.r << ", " 
+                << portal_pair.second.c << ") \n";
+    }
+    std::cout << "9 - Exit options\n"; 
+    int option = -1;
+    while (true) {
+        try {
+            option = readInt(0, 9);
+            break;
+        } catch (const std::out_of_range& e) {
+            std::cout << e.what() << "Press 9 to exit options." << std::endl;
+        }
+    }
+        
+    switch(option){
+        case 0: {
+        
+        } case 1: {
+        
+        } case 2: {
+        
+        } case 3: {
+        
+        } case 4: {
+        
+        } case 5: {
+        
+        } case 6: {
+        
+        } case 7: {
+        
+        } case 8: {
+        
+        } case 9: {
+            return;
+        }  
+    }
+}
+
+int main(int argc, char** argv) {
+    Options o;
+    while (true) {
+        std::cout << "Press p to start game, o to set options, or q to quit.\n";
+        try {
+            setOptions(o);
+            break;
+        } catch (const std::exception& e) {
+            std::cout << "Invalid input. Please try again." << std::endl;
+        }
+    }
+    Board b(o.height, o.width, o.speed, o.starting_length, o.enlargement, o.borders_on, o.invincible, o.portals);
+    View v(o.height, o.width, &b, o.fps);
     std::thread refresh_screen(&View::displayScreen, &v);
     std::thread move_snake(&Board::moveSnake, &b);
 

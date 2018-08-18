a3f6f7eca17af86cb16fe8b98bd42b5bd1ea32f6
Umer Amjad <m.amjad.umer@gmail.com>
2018-08-17 21:30:20 -0400

    Implemented ability to change options. Need to fine tune to allow replaying a game after quitting or exiting and change multiple options more easily

--- main.cc
+++ main.cc
@@ -48,15 +48,19 @@ int readInt(int lower, int upper){
     return stoi(input);
 }
 
+std::string boolString(bool b) {
+    return b ? "On" : "Off";
+}
+
 void setOptions(Options& o) {
-    std::cout << "Current options. Enter the associated number to edit \n";
+    std::cout << "Current options. Enter the associated number to toggle option or edit \n";
     std::cout << "0 - Height: " << o.height << '\n';
     std::cout << "1 - Width: " << o.width << '\n'; 
     std::cout << "2 - Snake speed: " << o.speed << '\n';
     std::cout << "3 - Snake starting length: " << o.starting_length << '\n';
     std::cout << "4 - Enlargement factor: " << o.enlargement << '\n';
-    std::cout << "5 - Borders: " << o.borders_on << '\n';
-    std::cout << "6 - Invincibility mode: " << o.invincible << '\n';
+    std::cout << "5 - Borders: " << boolString(o.borders_on) << '\n';
+    std::cout << "6 - Invincibility mode: " << boolString(o.invincible) << '\n';
     std::cout << "7 - Frames per second: " << o.fps << '\n';
     std::cout << "8 - Portal pairs, " << o.portals.size() << ": \n";
     for (const auto& portal_pair : o.portals) {
@@ -69,35 +73,77 @@ void setOptions(Options& o) {
     int option = -1;
     while (true) {
         try {
-            option = readInt(0, 9);
+            switch(readInt(0, 9)){
+                case 0: {
+                    std::cout << "Enter height \n";
+                    o.height = readInt(2, 500);
+                    break;
+                } case 1: {
+                    std::cout << "Enter width \n";
+                    o.width = readInt(2, 500); 
+                    break;
+                } case 2: {
+                    std::cout << "Enter snake speed (blocks per second) \n";
+                    o.speed = readInt(1, 100); 
+                    break;
+                } case 3: {
+                    std::cout << "Enter snake starting length (in blocks) \n";
+                    o.starting_length = readInt(1, o.height * o.width); 
+                    break;
+                } case 4: {
+                    std::cout << "Enter enlargement factor (blocks per food eaten) \n";
+                    o.enlargement = readInt(1, 100); 
+                    break;
+                } case 5: {
+                    o.borders_on = !o.borders_on;
+                    std::cout << "Borders are now: " << boolString(o.borders_on) << '\n';
+                    break;
+                } case 6: {
+                    o.invincible = !o.invincible;
+                    std::cout << "Invincibility mode is now: " << boolString(o.borders_on) << '\n';
+                    break;
+                } case 7: {
+                    std::cout << "Enter frames per second \n";
+                    o.fps = readInt(1, 100); 
+                    break;
+                } case 8: {
+                    std::cout << "Enter the number of portal pairs\n";
+                    int num_portals = readInt(0, o.height * o.width);
+                    std::set<Portal> all_portals; 
+                    std::set<std::pair<Portal, Portal>> new_portals;
+                    for (int i = 0; i < num_portals; ++i) {
+                        std::cout << "Enter the row of the first portal\n";
+                        int first_r = readInt(0, o.height - 1);
+                        std::cout << "Enter the column of the first portal\n";
+                        int first_c = readInt(0, o.width - 1);
+                        Portal first{first_r, first_c};
+                        if (all_portals.find(first) != all_portals.end()) {
+                            throw std::out_of_range("You entered a position where a portal already exists. Portals will not be changed. ");
+                        } else {
+                            all_portals.insert(first);
+                        }
+                        std::cout << "Enter the row of the second portal\n";
+                        int second_r = readInt(0, o.height - 1);
+                        std::cout << "Enter the column of the second portal\n";                        
+                        int second_c = readInt(0, o.width - 1);
+                        Portal second{second_r, second_c};
+                        if (all_portals.find(second) != all_portals.end()) {
+                            throw std::out_of_range("You entered a position where a portal already exists. Portals will not be changed. ");
+                        } else {
+                            all_portals.insert(second);
+                        }
+                        new_portals.insert({first, second});
+                    }
+                    o.portals = new_portals;
+                    break;
+                } case 9: {
+                    return;
+                }  
+            }
             break;
         } catch (const std::out_of_range& e) {
             std::cout << e.what() << "Press 9 to exit options." << std::endl;
         }
-    }
-        
-    switch(option){
-        case 0: {
-        
-        } case 1: {
-        
-        } case 2: {
-        
-        } case 3: {
-        
-        } case 4: {
-        
-        } case 5: {
-        
-        } case 6: {
-        
-        } case 7: {
-        
-        } case 8: {
-        
-        } case 9: {
-            return;
-        }  
     }
 }
 
